07514f6eb9c8c253952f2eb243fa378e7ee03690
itc-n25010 <n25010@std.it-college.ac.jp>
2025-05-28 02:52:47 +0000

    challenge5-04.py 修正

--- Chapter05/challenge5-04.py
+++ Chapter05/challenge5-04.py
@@ -1,10 +1,11 @@
 fav={
         "ブルアカ":"陸八魔アル",
         "学マス":"篠澤広",
-        "プロセカ":"日野森志歩",
-    }
+}
 
-ans=input("ブルアカor学マスorプロセカ:")
+ans=input("ブルアカor学マス:")
 if ans in fav:
     res=fav[ans]
     print(res)
+else:
+    print("わかりません")
